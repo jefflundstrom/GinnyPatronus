10d2dafe7da237cf0e4d59119e7d191d84b4bc07
jefflundstrom <jefflundstrom@users.noreply.github.com>
2024-01-23 14:55:12 -0500

    Update GinnyPareonus.ino

--- GinnyPareonus.ino
+++ GinnyPareonus.ino
@@ -2,173 +2,179 @@
 #include <WebServer.h>
 #include <time.h>
 #include <AutoConnect.h>
+#include <FS.h>
+#include <ArduinoJson.h> 
 
-#define LED_PIN 13 // Change to the GPIO pin you're using
-#define FADE_TIME 3000 // Total time for one fade in/out cycle in milliseconds
+#define LED_PIN 13
+#define FADE_TIME 3000
 #define PI 3.14159265
 
-
 WebServer Server;
-AutoConnect       Portal(Server);
-AutoConnectConfig Config;       // Enable autoReconnect supported on v0.9.4
-AutoConnectAux    Timezone;
+AutoConnect Portal(Server);
+AutoConnectConfig Config;
+AutoConnectAux Timezone;
 static const char *wd[7] = { "Sun","Mon","Tue","Wed","Thr","Fri","Sat" };
 char dateTime[40];
-time_t  t= time(NULL);
+time_t t= time(NULL);
 struct tm *tm;
 int LEDVALUE = 0;
+
+String onTime = "1700", offTime = "2359";
+bool hourlyflash = true, useSchedule = true;
+
 void rootPage() {
-   String content =
-    "<html>"
-    "<head>"
-    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
-    "<script  type=\"text/javascript\">"
-    "function toggleLED() {"
-    "  var xhr = new XMLHttpRequest();"
-    "  xhr.open('GET', '/toggleLED', true);"
-    "  xhr.send();"
-    "}"
-    "function setSchedule() {"
-    "  var xhr = new XMLHttpRequest();"
-    "  var onTime = document.getElementById('onTime').value;"
-    "  var offTime = document.getElementById('offTime').value;"
-    "  xhr.open('GET', '/setSchedule?on=' + onTime + '&off=' + offTime, true);"
-    "  xhr.send();"
-    "}"
-    "</script>"
-     "<style>"
-        "body {"
-            "background-color: black;"
-            "text-align: center;"
-            "color: white;"
-        "}"
-      "</style>"
-      "<Title>Ginny Weasley's Patronus</Title>"
-    "</head>"
-    "<body>"
-    "<center><img src=\"https://th.bing.com/th/id/OIP.SbXooHPcEGXqwVazyV7-rgHaFD?rs=1&pid=ImgDetMain\"><center>"
-    "<p>The words Expecto Patronum literally translate to \"I expect a guardian,\" in Latin. The patronus is also a reminder of hope and love as it comes in the form of the wizard or witch's spirit guardian.</p>"
-    "<p><a href = \"https://en.wikipedia.org/wiki/Ginny_Weasley\" style=\"color: lightgray;\"><h1>Ginny Weasley's</h1></a> Patronus is a Horse. The Horse Patronus symbolizes Ginny's strong will, independence, and free spirit, much like a wild horse. Horses, throughout history and in many cultures, have been symbolic of power, grace, beauty, nobility, strength, and freedom. All of these attributes match Ginny's character as she grows from a shy young girl into a confident, brave, and key player in the fight against Voldemort.<p>"
+  String content =
+    "<html><head><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><script type=\"text/javascript\">"
+    "function toggleLED(){var xhr=new XMLHttpRequest();xhr.open('GET','/toggleLED',true);xhr.send();}"
+    "function setSchedule(){var xhr=new XMLHttpRequest();var onTime=document.getElementById('onTime').value;"
+    "var offTime=document.getElementById('offTime').value;var hourlyflash=document.getElementById('patronusHourly').checked;"
+    "var useSchedule=document.getElementById('useSchedule').checked;"
+    "xhr.open('GET','/setSchedule?onTime='+onTime+'&offTime='+offTime+'&hourlyflash='+hourlyflash+'&useSchedule='+useSchedule,true);"
+    "xhr.send();}</script><style>body{background-color:black;text-align:center;color:white;}</style>"
+    "<Title>Ginny Weasley's Patronus</Title></head><body><center><img src=\"https://th.bing.com/th/id/OIP.SbXooHPcEGXqwVazyV7-rgHaFD?rs=1&pid=ImgDetMain\">"
+    "<center><p>The words Expecto Patronum translate to \"I expect a guardian,\" in Latin. The patronus is a reminder of hope and love as it comes in the form of the wizard or witch's spirit guardian.</p>"
+    "<p><a href=\"https://en.wikipedia.org/wiki/Ginny_Weasley\" style=\"color:lightgray;\"><h1>Ginny Weasley's</h1></a> Patronus is a Horse. The Horse Patronus symbolizes Ginny's strong will, independence, and free spirit, much like a wild horse. Horses, throughout history and in many cultures, have been symbolic of power, grace, beauty, nobility, strength, and freedom. All of these attributes match Ginny's character as she grows from a shy young girl into a confident, brave, and key player in the fight against Voldemort.</p>"
     "<center><iframe loading=\"lazy\" width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/eG6ujqx_Mu0\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe></center>"
     "<h3 align=\"center\" style=\"color:gray;margin:10px;\">{{DateTime}}</h3>"
-    "<p style=\"text-align:center;\">Reload the page to update the time.</p>"
-    "<button onclick=\"toggleLED()\">Toggle LED</button>"
-    "<p>Set On Time: <input type='time' id='onTime'></p>"
-    "<p>Set Off Time: <input type='time' id='offTime'></p>"
+    "<button onclick=\"toggleLED()\">Toggle Light</button>"
+    "<p><label for=\"patronusHourly\">Patronus Hourly:</label><input type=\"checkbox\" id=\"patronusHourly\" name=\"patronusHourly\" {{HOURLYCHECK}}></p>"
+    "<p><label for=\"useSchedule\">Use Scheduled Times Below:</label><input type=\"checkbox\" id=\"useSchedule\" name=\"useSchedule\" {{USESCHEDULE}}></p>"
+    "<p>Set On Time: <input type='time' id='onTime' value='{{ONTIME}}'></p>"
+    "<p>Set Off Time: <input type='time' id='offTime' value='{{OFFTIME}}'></p>"
     "<button onclick=\"setSchedule()\">Set Schedule</button>"
-    "<p style=\"padding-top:15px;text-align:center\">" AUTOCONNECT_LINK(COG_24) "</p>"
+    "<p style=\"padding-top:15px;text-align:center\">" AUTOCONNECT_LINK(COG_24) "</p></body></html>";
 
-    "</body>"
-    "</html>";
-    t= time(NULL);
-    tm = localtime(&t);
-    sprintf(dateTime, "%02d/%02d/%04d(%s) %02d:%02d:%02d.",
-      tm->tm_mon + 1, tm->tm_mday, tm->tm_year + 1900,
-      wd[tm->tm_wday],
-      tm->tm_hour, tm->tm_min, tm->tm_sec);
-    content.replace("{{DateTime}}", String(dateTime));
-    Server.send(200, "text/html", content);
+  t = time(NULL);
+  tm = localtime(&t);
+  sprintf(dateTime, "%02d/%02d/%04d(%s) %02d:%02d:%02d.", tm->tm_mon + 1, tm->tm_mday, tm->tm_year + 1900, wd[tm->tm_wday], tm->tm_hour, tm->tm_min, tm->tm_sec);
+  content.replace("{{DateTime}}", String(dateTime));
+  content.replace("{{HOURLYCHECK}}", hourlyflash ? "Checked" : "");
+  content.replace("{{USESCHEDULE}}", useSchedule ? "Checked" : "");
+  String timeToDisplay = onTime.substring(0, 2) + ":" + onTime.substring(2, 4);
+  content.replace("{{ONTIME}}",  timeToDisplay);
+  timeToDisplay = offTime.substring(0, 2) + ":" + offTime.substring(2, 4); 
+  content.replace("{{OFFTIME}}", timeToDisplay);
+  Server.send(200, "text/html", content);
 }
 
 int baseBrightness = 0;
-void performPatronus()
-{
 
+void performPatronus() {
   Serial.println("Performing Patronus");
-
-  for(int i = 0; i < 260; i += 20)
-  {
+  for(int i = 0; i < 260; i += 20) {
+    Portal.handleClient();
     baseBrightness = i;
     unsigned long startTime = millis();
     while (millis() - startTime < FADE_TIME) {
       float time = (millis() - startTime) / (float)FADE_TIME;
-      float brightness = (sin(time * 2 * PI - PI/2) + 1) / 2; // Ranges from 0 to 1
-
+      float brightness = (sin(time * 2 * PI - PI/2) + 1) / 2; 
       analogWrite(LED_PIN, min(255, (int)(brightness * 255) + baseBrightness));
       delay(10);
     }
   }
 }
 
+void readAndParseJSON() {
+  File file = SPIFFS.open("/mysettings.json", "r");
+  if (!file) return;
+  const size_t bufferSize = 1000;
+  DynamicJsonDocument jsonDoc(bufferSize);
+  DeserializationError error = deserializeJson(jsonDoc, file);
+  if (error) return;
+  String onTimeValue = jsonDoc["timeOn"];
+  onTime = onTimeValue;
+  String offTimeValue = jsonDoc["timeOff"];
+  offTime = offTimeValue;
+  hourlyflash = jsonDoc["hourly"];
+  useSchedule = jsonDoc["timer"];
+  file.close();
+}
+
+void saveDataToJSON() {
+  StaticJsonDocument<1000> jsonDoc;
+  jsonDoc["timeOn"] = onTime;
+  jsonDoc["timeOff"] = offTime;
+  jsonDoc["hourly"] = hourlyflash;
+  jsonDoc["timer"] = useSchedule;
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
+  File file = SPIFFS.open("/mysettings.json", "w");
+  if (file) {
+    file.println(jsonString);
+    file.close();
+  }
+}
+
 void setup() {
   delay(1000);
   Serial.begin(115200);
-  Serial.println();
-  
   pinMode(LED_PIN, OUTPUT);
-
-  // Enable saved past credential by autoReconnect option,
-  // even once it is disconnected.
+  if (SPIFFS.begin()) readAndParseJSON();
   Config.autoReconnect = true;
   Portal.config(Config);
-
-  // Behavior a root path of ESP8266WebServer.
   Server.on("/", rootPage);
-
-  // Handler for toggling the LED
   Server.on("/toggleLED", []() {
-
-      Serial.println("Button clicked");
-
-      //If we are going on, we need to perform the Patronus.
-      if(LEDVALUE == 0)
-      {
-        performPatronus();
-        LEDVALUE = 255;
-      }
-      else
-      {
-        LEDVALUE = 0;
-      }
-
-      Server.send(200, "text/plain", "Toggled");
+    if (LEDVALUE == 0) {
+      performPatronus();
+      LEDVALUE = 255;
+    } else {
+      LEDVALUE = 0;
+    }
+    Server.send(200, "text/plain", "Toggled");
   });
-
-  // Handler for setting the schedule
   Server.on("/setSchedule", []() {
-    // Retrieve on and off times
-    Serial.println("Schedule Button clicked");
-    String onTime = Server.arg("on");
-    String offTime = Server.arg("off");
+    String newTime = Server.arg("onTime");
+    if (newTime.length() >= 5 && newTime.indexOf(':') != -1)
+      onTime = newTime.substring(0, 2) + newTime.substring(3);
 
-    Serial.println(onTime);
-    Serial.println(offTime);
+    newTime = Server.arg("offTime");
+    if (newTime.length() >= 5 && newTime.indexOf(':') != -1)
+      offTime = newTime.substring(0, 2) + newTime.substring(3);
 
-    // Add logic to store and handle these times
+    hourlyflash = Server.arg("hourlyflash") == "true";
+    useSchedule = Server.arg("useSchedule") == "true";
+    saveDataToJSON();
     Server.send(200, "text/plain", "Schedule Set");
   });
-
-  // Establish a connection with an autoReconnect option.
-  if (Portal.begin()) {
-    Serial.println("WiFi connected: " + WiFi.localIP().toString());
-  }
-
-   // Retrieve the value of AutoConnectElement with arg function of WebServer class.
-  // Values are accessible with the element name.
+  if (Portal.begin()) Serial.println("WiFi connected: " + WiFi.localIP().toString());
   configTime(-5 * 3600, 0, "north-america.pool.ntp.org");
 }
 
+bool displayOnce = true;
 
 void loop() {
   Portal.handleClient();
-  
   analogWrite(LED_PIN, LEDVALUE);
+  if (hourlyflash || useSchedule) {
+    t = time(NULL);
+    tm = localtime(&t);
+    if (tm->tm_year > 100) {
+      sprintf(dateTime, "%02d/%02d/%04d(%s) %02d:%02d:%02d.",
+        tm->tm_mon + 1, tm->tm_mday, tm->tm_year + 1900,
+        wd[tm->tm_wday],
+        tm->tm_hour, tm->tm_min, tm->tm_sec);
 
-  t= time(NULL);
-  tm = localtime(&t);
+      if (displayOnce) {
+        displayOnce = false;
+      }
+      if (hourlyflash && tm->tm_min == 0) {
+        performPatronus();
+      } else if (useSchedule) {
+        int currentHour = tm->tm_hour;
+        int currentMinute = tm->tm_min;
+        int currentTime = (currentHour * 100) + currentMinute;
+        int ontime = atoi(onTime.c_str());
+        int offtime = atoi(offTime.c_str());
 
-  //On the hour we will perform the patronus.
-  if(tm->tm_year > 100)
-  {
-    if(tm->tm_min == 0)
-    {
-     Serial.println("TOP OF THE HOUR");
-     sprintf(dateTime, "%02d/%02d/%04d(%s) %02d:%02d:%02d.",
-     tm->tm_mon + 1, tm->tm_mday, tm->tm_year + 1900,
-     wd[tm->tm_wday],
-     tm->tm_hour, tm->tm_min, tm->tm_sec);
-     Serial.println(dateTime);
-     performPatronus();
+        if (currentTime == ontime) {
+          performPatronus();
+          LEDVALUE = 255;
+        } else if (currentTime == offtime) {
+          LEDVALUE = 0;
+          analogWrite(LED_PIN, LEDVALUE);
+          delay(60000);
+        }
+      }
     }
   }
 }
